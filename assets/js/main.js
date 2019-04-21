8064c0a95e21d4b8753a21091319331ddb609047
Melle Dijkstra <melle210202@gmail.com>
2019-04-21 20:07:36 +0200

    Test file and retrieve exif data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 
 console.log('working');
 
-$(".pure-img").each(function() {
-	var _this = this;
-	EXIF.getData(this, function() {
-		var exposureTime = EXIF.getAllTags(this);
-		console.log(exposureTime);
+$(window).on("load", function() {
+	console.log('loaded');
+	$(".pure-img").each(function() {
+		var _this = $(this);
+		EXIF.getData(this, function() {
+			var all = EXIF.getAllTags(this);
+			var ISO = EXIF.getTag(this, "ISOSpeedRatings");
+			var expTime = EXIF.getTag(this, "ExposureTime");
+			var aperture = EXIF.getTag(this, "FNumber");
+			_this.parent().append("<div>ISO: "+ISO+" | exposure: "+expTime.numerator+"/"+expTime.denominator+" ("+expTime+") | f/"+aperture+"</div>");
+		});
 	});
 });
